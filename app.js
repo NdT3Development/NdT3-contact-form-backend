728399c773c2ab528251210e1cb4e7231767c213
NdT3Development <nicholis.dutoit@gmail.com>
2017-08-24 18:59:15 +0200

    Changed message to JSON output

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ smtpTrans.verify(function(error, success) {
 var apiRatelimiter = new RateLimit({
   windowMs: 7500, // 7.5 second window
   max: 5, // start blocking after 5 requests
-  message: "Ratelimited"
+  message: {"responseCode": 1, "responseDesc": "Ratelmited (too many requests)"}
 });
 
 app.get('/onlinecheck', apiRatelimiter, function(req,res) {
