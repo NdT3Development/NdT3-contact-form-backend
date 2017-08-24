087957fbd6a92734c3791d08bb4337fe7adb9719
NdT3Development <nicholis.dutoit@gmail.com>
2017-08-24 19:42:00 +0200

    Enabled headers

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ var apiRatelimiter = new RateLimit({
   max: 5, // start blocking after 5 requests
   delayAfter: 0, // disable slow down of requests
   delayMs: 0,
+  headers: true,
   handler: function (req, res, options) {
     if (options.headers) {
       res.setHeader('Retry-After', Math.ceil(options.windowMs / 1000));
